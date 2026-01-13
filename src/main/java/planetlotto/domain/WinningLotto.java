741bb6c3ce8ca8827c2c334695ec05dddf5f868c
goeka <goeka1337@gmail.com>
2026-01-14 01:23:49 +0900

    refactor: WinningLotto의 bonusNumber 필드를 private으로 변경

--- src/main/java/planetlotto/domain/WinningLotto.java
+++ src/main/java/planetlotto/domain/WinningLotto.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class WinningLotto {
     private List<Integer> numbers;
-    int bonusNumber;
+    private int bonusNumber;
 
     public WinningLotto(List<Integer> numbers) {
         this.numbers = numbers;
